2c6a5fff1aeee85acf47526656ba2a9d9beec925
Prasad Jayasundara <30996052+prasad-jaya@users.noreply.github.com>
2021-05-12 18:53:55 +0530

    Update APIs.js

--- src/APIs/APIs.js
+++ src/APIs/APIs.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-const Key = 'AIzaSyBMvcFhHvjBdK28o2kyJuca4Iztyc5vxHI';
-
 export default axios.create({
     baseURL:'http://localhost/CRUD/cru/index.php',
     headers:  { 'Content-Type': 'application/x-www-form-urlencoded' },
